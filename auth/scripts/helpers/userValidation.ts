5f94a086085b8507f0c6838e0459a79a70b9c43d
egor-mihailove <jumpeebunee@gmail.com>
2023-05-23 16:41:01 +0300

    feat: add password encryption

--- auth/scripts/helpers/userValidation.ts
+++ auth/scripts/helpers/userValidation.ts
@@ -1,16 +1,24 @@
+import { VALID_CHARACTERS } from "../data/data";
 import { ApiError } from "../errors";
 
-const checkValidity = (len: number, field: string, min: number, max: number): void => {
-    if (len === 0) {
+const checkValidity = (input: string, field: string, min: number, max: number): void => {
+    if (input.length === 0) {
         throw ApiError.ValidationError(`${field} can't be blank`);
-    } else if (len > max) {
+    } else if (input.length > max) {
         throw ApiError.ValidationError(`${field} is too long (maximum is ${max} characters)`);
-    } else if (len < min) {
+    } else if (input.length < min) {
         throw ApiError.ValidationError(`${field} is too short (minimum is ${min} characters)`);
     }
+
+    input.split('').forEach(item => {
+        if (!VALID_CHARACTERS.includes(item.toLocaleLowerCase())) {
+            throw ApiError.ValidationError(`${field} contains invalid characters (use only A-Z and 0-9)`);
+        }
+    })
+
 }
 
 export const userValidation = (username: string, password: string): void => {
-    checkValidity(username.trim().length, "Username", 5, 30);
-    checkValidity(password.trim().length, "Password", 6, 128);
+    checkValidity(username.trim(), "Username", 5, 30);
+    checkValidity(password.trim(), "Password", 6, 128);
 }
--- auth/scripts/server/server.ts
+++ auth/scripts/server/server.ts
@@ -1,15 +1,17 @@
 import { ApiError } from "../errors";
+import { encryptPassword } from "../helpers/hashPassword";
 import { IUser } from "../types";
 
 const credentials: IUser[] = [];
 
 const login = (username: string, password: string): Promise<IUser> => {
-    const foundUser = credentials.some(user => user.username === username && user.password === password);
+    const hashedPassword = encryptPassword(password);
+    const foundUser = credentials.some(user => user.username === username && user.password === hashedPassword);
 
     return new Promise((res, rej) => {
         setTimeout(() => {
             if (foundUser) {
-                res({ username, password });
+                res({ username, password: hashedPassword });
             } else {
                 rej(ApiError.AuthError('Incorrect username or password'));
             }
@@ -23,8 +25,9 @@ const register = (username: string, password: string): Promise<IUser> => {
     return new Promise((res, rej) => {
         setTimeout(() => {
             if (!isFoundUser) {
-                res({ username, password });
-                credentials.push({ username, password });
+                const hashedPassword = encryptPassword(password);
+                res({ username, password: hashedPassword });
+                credentials.push({ username, password: hashedPassword});
             } else {
                 rej(ApiError.AuthError('Username is already taken'));
             }
